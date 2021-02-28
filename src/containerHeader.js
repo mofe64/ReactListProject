09a40f7471efcbc119a4a6a959559bcbb0c1fc68
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-02-28 15:57:01 +0100

    search component

--- src/containerHeader.js
+++ src/containerHeader.js
@@ -1,4 +1,7 @@
 import './css/containerHeader.css';
+import Search from './search';
 const ContainerHeader = ()=>
-    <div className="container-header"></div>
+    <div className="container-header">
+        <Search></Search>
+    </div>
 export default ContainerHeader;
---
+++ src/search.js
@@ -0,0 +1,8 @@
+import './css/search.css';
+const Search = () => 
+        <div className="searchBox">
+            <input type="search" placeholder="filter results" className="searchBox"></input>
+            <button>Clear</button>
+        </div>
+
+export default Search;
